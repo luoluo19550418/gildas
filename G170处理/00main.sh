8182866ea86da74beafcc5a2336095771149f1ba
csluo19550418 <30993375+luoluo19550418@users.noreply.github.com>
2020-07-08 11:46:57 +0800

    Add files via upload

---
+++ G170处理/00main.sh
@@ -0,0 +1,12 @@
+class @01hebur.class sou.txt 16
+class @02basergd.class
+class @03ave.class
+class @04basesum.class
+class @05pinsum.class
+class @06mosaic.class
+class @07basemosaic.class
+class @07modifyfreq.class
+class @08outdat.class
+greg @09plot_sigdat.greg
+class @10pvstrip.class
+greg @10pvplot.greg
---
+++ G170处理/09plot_sigdat_gla.sh
@@ -0,0 +1,3 @@
+greg @09plot_sig12_gla.greg
+greg @09plot_sig13_gla.greg
+greg @09plot_sig18_gla.greg
